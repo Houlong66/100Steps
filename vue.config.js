a19ec523ed7b1dda79890889104f679acb114201
515491847 <515491847@qq.com>
2018-11-26 21:50:06 +0800

    Modify config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
  // 基本路径
  baseUrl: '/',
  // 输出文件目录
- outputDir: 'test',
+ outputDir: '../dist',
+ assetsDir: 'static',
  // eslint-loader 是否在保存的时候检查
  lintOnSave: true,
  // use the full build with in-browser compiler?
